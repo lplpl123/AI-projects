5f0dc5f7afd63b7f8c9998b936ef3786b3750d02
lplpl123 <758826560@qq.com>
2023-02-18 21:17:45 +0800

    20230218

--- recognize handwritings/main.py
+++ recognize handwritings/main.py
@@ -4,14 +4,14 @@
 # 推理脚本
 
 import pandas as pd
-from torchvision import datasets
+from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
-# 读取手写数据集
+# 读取手写数据集, 总共有60000张图片
 train_data = datasets.MNIST(root="./MNIST",
                             train=True,
                             download=False)
-
-for i in train_data:
-    print(i)
+test_data = datasets.MNIST(root="./MNIST",
+                           train=False,
+                           download = False)
--- recognize handwritings/test.py
+++ recognize handwritings/test.py
@@ -1,4 +1,6 @@
+import numpy as np
 from PIL import Image
 
-data = Image.open("C:/Users/75882/Desktop/play/图片/别人/1.jpg")
-print(data)
+image = Image.open("C:/Users/75882/Desktop/play/图片/别人/1.jpg")
+image_array = np.array(image)
+print(image_array.shape)
