d540b9a01b492b7692fbede083a17a6d1307244f
lplpl123 <758826560@qq.com>
2023-02-19 14:42:12 +0800

    20230219

--- recognize handwritings/main.py
+++ recognize handwritings/main.py
@@ -8,10 +8,18 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 
+# 把读取到的图片转换为tensor
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
 # 读取手写数据集, 总共有60000张图片
 train_data = datasets.MNIST(root="./MNIST",
                             train=True,
+                            transform=transform,
                             download=False)
 test_data = datasets.MNIST(root="./MNIST",
                            train=False,
+                           transform=transform,
                            download = False)
+
+
