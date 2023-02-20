1ae4544521962fce8d9828809675b367aa26d3cd
lplpl123 <758826560@qq.com>
2023-02-20 19:58:00 +0800

    20230220

--- recognize handwritings/main.py
+++ recognize handwritings/main.py
@@ -4,6 +4,7 @@
 # 推理脚本
 
 import pandas as pd
+import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
@@ -21,5 +22,9 @@ test_data = datasets.MNIST(root="./MNIST",
                            train=False,
                            transform=transform,
                            download = False)
+
+class CNN():
+    def __init__(self):
+        self.conv2d = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3)
 
 
