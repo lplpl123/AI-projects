835f2808e7c3bae5af8db6bce982fa6ef1affd58
p30030010 <pugejing@huawei.com>
2023-02-21 16:20:04 +0800

    20230221

--- recognize handwritings/main.py
+++ recognize handwritings/main.py
@@ -22,9 +22,35 @@ test_data = datasets.MNIST(root="./MNIST",
                            train=False,
                            transform=transform,
                            download = False)
+train_loader = DataLoader(dataset=train_data, batch_size=10000, shuffle=True)
+for loader in train_loader:
+    print(loader[1].requires_grad)
+
 
 class CNN():
     def __init__(self):
         self.conv2d = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3)
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=1)
+        self.linear = torch.nn.Linear(24 * 24, 10)
+        self.loss_function = torch.nn.MSELoss()
+        self.optimizer = None # todo
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        x = self.maxpool(x)
+        x = x.reshape(1, -1)
+        x = self.linear(x)
+        return x
+
+    def train(self):
+        for epoch in range(100):
+            for loader in train_loader:
+                x = loader[0]
+                y_label = loader[1]
+                y = self.forward(x)
+                loss = self.loss_function(y, y_label)
+
+                loss.backward()
+
 
 
--- recognize handwritings/test.py
+++ recognize handwritings/test.py
@@ -1,6 +1,1 @@
-import numpy as np
-from PIL import Image
 
-image = Image.open("C:/Users/75882/Desktop/play/图片/别人/1.jpg")
-image_array = np.array(image)
-print(image_array.shape)
