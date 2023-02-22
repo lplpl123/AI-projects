39f0d77afe7ba93e0a1a2de8c1b38eda3d3c7fe4
lplpl123 <758826560@qq.com>
2023-02-22 17:04:48 +0800

    20230222

--- recognize handwritings/main.py
+++ recognize handwritings/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
+from torch import nn
 
 
 # 把读取到的图片转换为tensor
@@ -23,34 +24,40 @@ test_data = datasets.MNIST(root="./MNIST",
                            transform=transform,
                            download = False)
 train_loader = DataLoader(dataset=train_data, batch_size=10000, shuffle=True)
-for loader in train_loader:
-    print(loader[1].requires_grad)
 
 
-class CNN():
+# 构建CNN模型
+class CNN(nn.Module):
     def __init__(self):
+        super(CNN, self).__init__()
         self.conv2d = torch.nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3)
         self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=1)
         self.linear = torch.nn.Linear(24 * 24, 10)
-        self.loss_function = torch.nn.MSELoss()
-        self.optimizer = None # todo
 
     def forward(self, x):
         x = self.conv2d(x)
         x = self.maxpool(x)
+        print(x.size())
         x = x.reshape(1, -1)
         x = self.linear(x)
         return x
 
-    def train(self):
-        for epoch in range(100):
-            for loader in train_loader:
-                x = loader[0]
-                y_label = loader[1]
-                y = self.forward(x)
-                loss = self.loss_function(y, y_label)
 
-                loss.backward()
+# 训练模型脚本
+cnn = CNN()
+loss_function = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(cnn.parameters(), lr=0.1)
+
+for epoch in range(100):
+    for images in train_loader:
+        x = images[0]
+        y_label = images[1]
+        y = cnn.forward(x)
+        loss = loss_function(y, y_label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print("完成第{}轮训练，loss为{}".format(epoch, loss))
 
 
 
