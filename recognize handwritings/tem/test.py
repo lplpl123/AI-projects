b880024a3edca8d6796edb46f9bbaeee97d85bd7
p30030010 <pugejing@huawei.com>
2023-03-01 10:40:30 +0800

    20230301

--- recognize handwritings/tem/test.py
+++ recognize handwritings/tem/test.py
@@ -1,10 +1,13 @@
 from PIL import Image
 import numpy as np
+import torch
 
 
 img = Image.open('C:/Users/p30030010/Desktop/temp/kadilake.jpg').convert('L')
 img = img.resize((28, 28))
 data = np.array(img)
 data = data.reshape((1, 1, 28, 28))
+data = torch.Tensor(data)
 print(data.shape)
+print(type(data))
 print(data)
